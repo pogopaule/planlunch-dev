c9f3fc388742dcd2450fca4a49ed3249c4ab195d
pogopaule <pogopaule@gmx.de>
2014-08-29 17:31:57 +0200

    relocate folders

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #! /bin/zsh
 
-tmux new-session -d -s planlunch -c 'client' 'ember serve --proxy=http://localhost:8080/'
+tmux new-session -d -s planlunch -c '../client' 'ember serve --proxy=http://localhost:8080/'
 tmux set-option remain-on-exit
-tmux split-window -c 'server' 'nodemon index.js'
-tmux split-window -h -c 'server'
+tmux split-window -c '../server' 'nodemon index.js'
+tmux split-window -h -c '../server'
 tmux select-pane -U
-tmux split-window -h -c 'client'
+tmux split-window -h -c '../client'
 mvim
 tmux attach
