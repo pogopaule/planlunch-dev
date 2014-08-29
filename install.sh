54adff26bff3a7d96b4d709f6fadd58fc70054d6
pogopaule <pogopaule@gmx.de>
2014-08-29 15:33:17 +0200

    run npm install

--- install.sh
+++ install.sh
@@ -5,3 +5,13 @@ cd planlunch
 git clone git@github.com:pogopaule/planlunch-client.git client
 git clone git@github.com:pogopaule/planlunch-server.git server
 git clone git@github.com:pogopaule/planlunch-dev.git dev
+
+cd client
+npm install
+
+cd ../server
+npm install
+
+cd ..
+
+echo 'all set up. start by running "./dev/start.sh"'
