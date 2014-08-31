c4c67edf1f23c08083a01f54bff8c3b3e5fa87d9
pogopaule <pogopaule@gmx.de>
2014-08-31 18:14:04 +0200

    better startup experience

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@
 tmux new-session -d -s planlunch -c '../client' 'ember serve --proxy=http://localhost:8080/'
 tmux set-option remain-on-exit
 tmux split-window -c '../server' 'nodemon index.js'
-tmux split-window -h -c '../server'
+tmux split-window -h -c '../server' 'nodemon --exec npm test'
 tmux select-pane -U
-tmux split-window -h -c '../client'
-mvim
+tmux split-window -h -c '../client' 'ember test --server'
+tmux new-window -c '../client'
+tmux split-window -h -c '../server'
+tmux select-window -t 0
+cd ..
+mvim -p client server dev
 tmux attach
